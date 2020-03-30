e6126c779a1cf8b6ac34dc805901dfef67fc0862
Samantha Sanford <samanthawebdev@gmail.com>
2020-03-30 01:07:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  return programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  return programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -79,7 +84,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  return programmer_hash[:yukihiro_matsumoto]
     
 end
 

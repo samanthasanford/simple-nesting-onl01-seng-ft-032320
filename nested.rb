4f09957ee2a73560bd70e3704670821f80ef5c25
Samantha Sanford <samanthawebdev@gmail.com>
2020-03-30 01:12:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,10 +71,6 @@ def adding_matz
 
 	programmer_hash =
  		{
-				:yukihiro_matsumoto => {
-					:known_for => "Ruby",
-					:languages => ["LISP", "C"]
-				}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -88,7 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  return programmer_hash[:yukihiro_matsumoto]
+
+	programmer_hash[:yukihiro_matsumoto]={
+			known_for: "Ruby",
+			languages => ["LISP", "C"]
+	}
+	programmer_hash
 
 end
 
